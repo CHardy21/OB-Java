475bab87bdc26779c37af6317908520670611a64
CHardy <christianrh2000@hotmail.com>
2022-10-25 11:40:12 -0300

    Funtions

--- com.ob-java/src/com/ob/practicasJava/funciones.java
+++ com.ob-java/src/com/ob/practicasJava/funciones.java
@@ -3,7 +3,15 @@ package com.ob.practicasJava;
 public class funciones {
 
 	public static void main(String[] args) {
-			
+		
+	// 1. Funciones sin pase de parametros y sin retorno
+		
+	// 2. Funciones con pase de parametros y sin retorno
+		
+	// 3. Funciones sin pase de parametros y con retorno
+		
+	// 4. Funciones con pase de parametros y con retorno
+		
 		double precio = 100.1;
 		
 		System.out.println("El precio Neto es:        " + precio);
